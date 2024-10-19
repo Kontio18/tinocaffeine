26a0d6f8a006d02259900a5cffb96c905ab85050
EC2 Default User <ec2-user@ip-172-31-26-123.us-east-2.compute.internal>
2024-10-19 04:20:02 +0000

    updated git ignore

--- tino/http_to_https.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const httpPort = 80;
-
-http.createServer((req, res) => {
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-  res.end();
-}).listen(httpPort);
